4e14db2a031545ef3b0845e9ee939fede1bbe25d
Pwn <hi@pwn.buzz>
2016-06-15 08:12:18 +0800

    get karma working on CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,6 @@
 module.exports = function ( config ) {
   config.set( {
-    sauceLabs : {
-      startConnect : false ,
-      tunnelIdentifier : process.env.TRAVIS_JOB_NUMBER
-    } ,
-    captureTimeout : 0 ,
     frameworks : [ 'expect' , 'mocha' ] ,
-    files : [ 'lib/tagOf.js' , 'test/**/*.js' ]
+    files : [ './lib/**/*.js' , './test/**/*.js' ]
   } )
 }
