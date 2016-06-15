ee1d29732a3b1fc0778cdf9931ebbb94bca9bab6
Pwn <hi@pwn.buzz>
2016-06-15 08:03:37 +0800

    updated karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,10 @@
 module.exports = function ( config ) {
   config.set( {
+    sauceLabs : {
+      startConnect : false ,
+      tunnelIdentifier : process.env.TRAVIS_JOB_NUMBER
+    } ,
+    captureTimeout : 0 ,
     frameworks : [ 'expect' , 'mocha' ] ,
     files : [ 'lib/tagOf.js' , 'test/**/*.js' ]
   } )
