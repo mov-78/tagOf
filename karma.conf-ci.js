8e221213250cf72d3fefe00a0e87ce875f8eac2c
Pwn <hi@pwn.buzz>
2016-06-16 17:02:17 +0800

    using YAML

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -1,98 +1,10 @@
+var fs = require( 'fs' )
+var yaml = require( 'js-yaml' )
+
 module.exports = function ( config ) {
 
-  var customLaunchers = {
-    SL_CHROME : {
-      base : 'SauceLabs' ,
-      browserName : 'chrome'
-    } ,
-    SL_FIREFOX : {
-      base : 'SauceLabs' ,
-      browserName : 'firefox'
-    } ,
-    SL_IE_6 : {
-      base : 'SauceLabs' ,
-      browserName : 'internet explorer' ,
-      version : '6'
-    } ,
-    SL_IE_7 : {
-      base : 'SauceLabs' ,
-      browserName : 'internet explorer' ,
-      version : '7'
-    } ,
-    SL_IE_8 : {
-      base : 'SauceLabs' ,
-      browserName : 'internet explorer' ,
-      version : '8'
-    } ,
-    SL_IE_9 : {
-      base : 'SauceLabs' ,
-      browserName : 'internet explorer' ,
-      version : '9'
-    } ,
-    SL_IE_10 : {
-      base : 'SauceLabs' ,
-      browserName : 'internet explorer' ,
-      version : '10'
-    } ,
-    SL_IE_11 : {
-      base : 'SauceLabs' ,
-      browserName : 'internet explorer' ,
-      version : '11'
-    } ,
-    SL_IOS_SAFARI_7 : {
-      base : 'SauceLabs' ,
-      browserName : 'iphone' ,
-      version : '7.0'
-    } ,
-    SL_IOS_SAFARI_8 : {
-      base : 'SauceLabs' ,
-      browserName : 'iphone' ,
-      version : '8.0'
-    } ,
-    SL_IOS_SAFARI_9 : {
-      base : 'SauceLabs' ,
-      browserName : 'iphone' ,
-      version : '9.0'
-    } ,
-    SL_IOS_SAFARI_LATEST : {
-      base : 'SauceLabs' ,
-      browserName : 'iphone'
-    } ,
-    SL_ANDROID_4_0 : {
-      base : 'SauceLabs' ,
-      browserName : 'android' ,
-      version : '4.0'
-    } ,
-    SL_ANDROID_4_1 : {
-      base : 'SauceLabs' ,
-      browserName : 'android' ,
-      version : '4.1'
-    } ,
-    SL_ANDROID_4_2 : {
-      base : 'SauceLabs' ,
-      browserName : 'android' ,
-      version : '4.2'
-    } ,
-    SL_ANDROID_4_3 : {
-      base : 'SauceLabs' ,
-      browserName : 'android' ,
-      version : '4.3'
-    } ,
-    SL_ANDROID_4_4 : {
-      base : 'SauceLabs' ,
-      browserName : 'android' ,
-      version : '4.4'
-    } ,
-    SL_ANDROID_5_0 : {
-      base : 'SauceLabs' ,
-      browserName : 'android' ,
-      version : '5.0'
-    } ,
-    SL_ANDROID_LATEST : {
-      base : 'SauceLabs' ,
-      browserName : 'android'
-    }
-  }
+  var list = fs.readFileSync( './.saucelabs.yml' )
+  var customLaunchers = yaml.safeLoad( list )
 
   config.set( {
     frameworks : [ 'expect' , 'mocha' ] ,
