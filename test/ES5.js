d510ec5195f70d0fa69cfa95e37bd9032d3afc73
Pwn <hi@pwn.buzz>
2016-09-22 07:57:39 +0800

    update packages

--- test/ES5.js
+++ test/ES5.js
@@ -1,71 +1,71 @@
 describe( 'tagOf' , function () {
 
-  it( 'null' , function () {
-    expect( tagOf( null ) ).toBe( 'null' )
-  } )
+    it( 'null' , function () {
+        expect( tagOf( null ) ).toBe( 'null' )
+    } )
 
-  it( 'undefined' , function () {
-    expect( tagOf( void 0 ) ).toBe( 'undefined' )
-  } )
+    it( 'undefined' , function () {
+        expect( tagOf( void 0 ) ).toBe( 'undefined' )
+    } )
 
-  it( 'number' , function () {
-    expect( tagOf( 0 ) ).toBe( 'number' )
-    expect( tagOf( 0 / 0 ) ).toBe( 'number' )
-    expect( tagOf( +1 / 0 ) ).toBe( 'number' )
-    expect( tagOf( -1 / 0 ) ).toBe( 'number' )
-    expect( tagOf( new Number( 0 ) ) ).toBe( 'number' )
-    expect( tagOf( new Number( 0 / 0 ) ) ).toBe( 'number' )
-    expect( tagOf( new Number( +1 / 0 ) ) ).toBe( 'number' )
-    expect( tagOf( new Number( -1 / 0 ) ) ).toBe( 'number' )
-  } )
+    it( 'number' , function () {
+        expect( tagOf( 0 ) ).toBe( 'number' )
+        expect( tagOf( 0 / 0 ) ).toBe( 'number' )
+        expect( tagOf( +1 / 0 ) ).toBe( 'number' )
+        expect( tagOf( -1 / 0 ) ).toBe( 'number' )
+        expect( tagOf( new Number( 0 ) ) ).toBe( 'number' )
+        expect( tagOf( new Number( 0 / 0 ) ) ).toBe( 'number' )
+        expect( tagOf( new Number( +1 / 0 ) ) ).toBe( 'number' )
+        expect( tagOf( new Number( -1 / 0 ) ) ).toBe( 'number' )
+    } )
 
-  it( 'string' , function () {
-    expect( tagOf( '' ) ).toBe( 'string' )
-    expect( tagOf( new String( '' ) ) ).toBe( 'string' )
-  } )
+    it( 'string' , function () {
+        expect( tagOf( '' ) ).toBe( 'string' )
+        expect( tagOf( new String( '' ) ) ).toBe( 'string' )
+    } )
 
-  it( 'boolean' , function () {
-    expect( tagOf( true ) ).toBe( 'boolean' )
-    expect( tagOf( false ) ).toBe( 'boolean' )
-    expect( tagOf( new Boolean( true ) ) ).toBe( 'boolean' )
-    expect( tagOf( new Boolean( false ) ) ).toBe( 'boolean' )
-  } )
+    it( 'boolean' , function () {
+        expect( tagOf( true ) ).toBe( 'boolean' )
+        expect( tagOf( false ) ).toBe( 'boolean' )
+        expect( tagOf( new Boolean( true ) ) ).toBe( 'boolean' )
+        expect( tagOf( new Boolean( false ) ) ).toBe( 'boolean' )
+    } )
 
-  it( 'object' , function () {
-    expect( tagOf( {} ) ).toBe( 'object' )
-    expect( tagOf( new Object() ) ).toBe( 'object' )
-  } )
+    it( 'object' , function () {
+        expect( tagOf( {} ) ).toBe( 'object' )
+        expect( tagOf( new Object() ) ).toBe( 'object' )
+    } )
 
-  it( 'array' , function () {
-    expect( tagOf( [] ) ).toBe( 'array' )
-    expect( tagOf( new Array() ) ).toBe( 'array' )
-  } )
+    it( 'array' , function () {
+        expect( tagOf( [] ) ).toBe( 'array' )
+        expect( tagOf( new Array() ) ).toBe( 'array' )
+    } )
 
-  it( 'function' , function () {
-    expect( tagOf( function () {} ) ).toBe( 'function' )
-    expect( tagOf( new Function() ) ).toBe( 'function' )
-  } )
+    it( 'function' , function () {
+        expect( tagOf( function () {} ) ).toBe( 'function' )
+        expect( tagOf( new Function() ) ).toBe( 'function' )
+    } )
 
-  it( 'date' , function () {
-    expect( tagOf( new Date() ) ).toBe( 'date' )
-  } )
+    it( 'date' , function () {
+        expect( tagOf( new Date() ) ).toBe( 'date' )
+    } )
 
-  it( 'regexp' , function () {
-    expect( tagOf( /^/i ) ).toBe( 'regexp' )
-    expect( tagOf( new RegExp() ) ).toBe( 'regexp' )
-  } )
+    it( 'regexp' , function () {
+        expect( tagOf( /^/i ) ).toBe( 'regexp' )
+        expect( tagOf( new RegExp() ) ).toBe( 'regexp' )
+    } )
 
-  it( 'custom' , function () {
+    it( 'custom' , function () {
 
-    var Foo = function Foo() {}
-    var Bar = function () {}
-    function Baz() {}
+        var Foo = function Foo() {}
+        var Bar = function () {}
+        function Baz() {}
 
-    expect( tagOf( new Foo() ) ).toBe( 'foo' )
-    expect( tagOf( new Bar() ) ).toBe( 'object' )
-    expect( tagOf( new Baz() ) ).toBe( 'baz' )
-    expect( tagOf( new ( function () {} ) ) ).toBe( 'object' ) // eslint-disable-line no-extra-parens
+        expect( tagOf( new Foo() ) ).toBe( 'foo' )
+        expect( tagOf( new Bar() ) ).toBe( 'object' )
+        expect( tagOf( new Baz() ) ).toBe( 'baz' )
+        expect( tagOf( new ( function () {} ) ) ).toBe( 'object' ) // eslint-disable-line no-extra-parens
 
-  } )
+    } )
 
 } )
--- test/ES6.js
+++ test/ES6.js
@@ -1,24 +1,24 @@
 describe( 'tagOf' , function () {
 
-  it( 'symbol' , function () {
-    if ( typeof Symbol === 'function' ) {
-      expect( tagOf( Symbol( 'tagOf' ) ) ).toBe( 'symbol' )
-      expect( tagOf( Symbol[ 'for' ]( 'tagOf' ) ) ).toBe( 'symbol' )
-    }
-  } )
+    it( 'symbol' , function () {
+        if ( typeof Symbol === 'function' ) {
+            expect( tagOf( Symbol( 'tagOf' ) ) ).toBe( 'symbol' )
+            expect( tagOf( Symbol[ 'for' ]( 'tagOf' ) ) ).toBe( 'symbol' )
+        }
+    } )
 
-  it( 'set' , function () {
-    if ( typeof Set === 'function' && typeof WeakSet === 'function' ) {
-      expect( tagOf( new Set() ) ).toBe( 'set' )
-      expect( tagOf( new WeakSet() ) ).toBe( 'weakset' )
-    }
-  } )
+    it( 'set' , function () {
+        if ( typeof Set === 'function' && typeof WeakSet === 'function' ) {
+            expect( tagOf( new Set() ) ).toBe( 'set' )
+            expect( tagOf( new WeakSet() ) ).toBe( 'weakset' )
+        }
+    } )
 
-  it( 'map' , function () {
-    if ( typeof Map === 'function' && typeof WeakMap === 'function' ) {
-      expect( tagOf( new Map() ) ).toBe( 'map' )
-      expect( tagOf( new WeakMap() ) ).toBe( 'weakmap' )
-    }
-  } )
+    it( 'map' , function () {
+        if ( typeof Map === 'function' && typeof WeakMap === 'function' ) {
+            expect( tagOf( new Map() ) ).toBe( 'map' )
+            expect( tagOf( new WeakMap() ) ).toBe( 'weakmap' )
+        }
+    } )
 
 } )
